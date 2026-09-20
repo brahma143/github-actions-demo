9d23dda2c8820d07598b5a98dacfc232cfb8bd6d
Brahma Reddy Lingam <94044367+brahma143@users.noreply.github.com>
2026-09-20 22:56:58 +0530

    Add Google Cloud Storage bucket resource

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
+resource "google_storage_bucket" "my-bucket" {
+  name     = "gh-actions-demo-001"
+  location = "US"
+  project  = "tt-dev-001"
+  force_destroy = true
+  public_access_prevention = "enforced"
+}
 
